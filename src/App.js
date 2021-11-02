3e5549fb147a115073b73f177e3200d3d76c1770
analia mancuso <analiamancuso@live.com>
2021-11-02 10:19:59 -0300

    feature/textcomponent add text component and style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import CardInfo from './components/CardInfo/cardInfo';
 import Navbar from './components/Navbar/navbar';
+import TextComponent from "./components/TextComponent/TextComponent";
 
 function App() {
   const planetas = ["Mercurio", "Venus", "Tierra"]
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar title="Planets" elements={planetas}/>
+      <TextComponent planet="mars" description="Sarasa de marte sarasa de marte sarasa de marte sarasa de marte sarasa de marte sarasa de martesarasa de marte sarasa de marte" source="https://en.wikipedia.org/wiki/Mars"/>
       <CardInfo title="hola" description="como va"/>
     </div>
   );
---
+++ src/components/TextComponent/TextComponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './textComponent.scss';
+import arrow from './outbound_white_24dp.svg';
+
+//planet: en funcion de navbar
+//description: informacion a partir de 1 overview, 2 internal y 3 surface de cada planeta
+//source: link a wiki 1 overview, 2 internal y 3 surface de cada planeta
+
+
+const TextComponent = (props) => {
+    const {planet, description, source} = props
+
+    return (
+        <div class="planet-info">
+            <h2>{planet}</h2>
+            <p>{description}</p>
+            <h3>Source: <a href={source}>Wikipedia<img src={arrow}/></a></h3>
+        </div>
+    )
+}
+export default TextComponent;
